b2bcf1f339f663293f104f39b673615bc021ff2f
swapno963 <swapno963@gmail.com>
2023-12-25 20:07:27 +0600

    filter is done

--- blog_project/views.py
+++ blog_project/views.py
@@ -6,7 +6,7 @@ def home(request, category_slug = None):
     data = Post.objects.all()
     if category_slug is not None:
         category = Category.objects.get(slug = category_slug)
-        data = Post.objects.all()
+        data = Post.objects.filter(category = category)
 
     categories = Category.objects.all()
     return render(request,'home.html', {'data': data,'category':categories})
